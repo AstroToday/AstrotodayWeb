d35001a01b3cb6f2891795f3e139cd0b043a3184
Arush <astrotoday2022@gmail.com>
2023-12-23 11:53:50 +0530

    Login Modal ui completed(without errors and warnings)

--- src/utils/code.ts
+++
@@ -1,7 +0,0 @@
-export const factorial = (n: number): number => {
-  // 0! = 1, 1! = 1, 2! = 2, 3! = 6, 4! = 24, 5! = 120, 6! = 720, 7! = 5040, 8! = 40320, 9! = 362880, 10! = 3628800
-  if (n === 0) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-};
